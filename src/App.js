df813fb03dc8405b7d15f77450f9150de3740202
Gabor Kuncevics <gabor.kuncevics@info.nl>
2018-04-06 21:55:46 +0200

    Styling

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { fetchSymbols, changeAmount, calcRate, getGraph } from './actions';
 import './App.css';
 import { format } from 'date-fns';
 import coupleWithDog from './img/doggos_walk.png';
+import NumberFormat from 'react-number-format';
 
 class App extends Component {
   componentDidMount() {
@@ -16,59 +17,92 @@ class App extends Component {
   }
 
   render() {
+    const {
+      amount,
+      changeAmount,
+      symbols,
+      calcRate,
+      getGraph,
+      convertedValue,
+      targetCurrency,
+      historicalData
+    } = this.props;
     return (
       <div className="App">
         <div
           style={{
-            width: '50%',
             display: 'flex',
             alignItems: 'center',
-            flexDirection: 'column'
+            flexDirection: 'column',
+            width: '50%'
           }}
         >
-          <br />
-          <NumberInput onChange={this.props.changeAmount} />
-          <div style={{ width: '50%', marginBottom: '20px' }}>
-            <StatesField
-              disabled
-              selectValue="EUR"
-              options={[{ label: 'EUR - Euro', value: 'EUR' }]}
-            />
-          </div>
-          <div style={{ width: '50%', marginBottom: '20px' }}>
-            <StatesField selectValue="USD" options={this.props.symbols} />
-          </div>
-          <div style={{ marginBottom: '20px' }}>
-            <RaisedButton
-              label="Calc"
-              labelColor="#ffffff"
-              backgroundColor="#56D5C0"
-              onClick={() => this.props.calcRate(this.props.targetCurrency)}
-            />
-          </div>
-          <div>
-            <RaisedButton
-              label="Graph"
-              labelColor="#ffffff"
-              backgroundColor="#4568E5"
-              onClick={() => this.props.getGraph(this.props.targetCurrency)}
-            />
-          </div>
-          <div>
-            <p>{this.props.convertedValue}</p>
-          </div>
-          {this.props.historicalData.length > 0 && (
-            <div>
-              <AreaChart
-                xType={'time'}
-                axes
-                width={700}
-                height={500}
-                areaColors={['#F3AF75']}
-                data={[this.props.historicalData]}
+          <div style={{ width: '60%' }}>
+            <br />
+            <NumberInput onChange={changeAmount} value={amount} />
+            <div style={{ width: '80%', marginBottom: '20px' }}>
+              <StatesField
+                disabled
+                selectValue="EUR"
+                options={[{ label: 'EUR - Euro', value: 'EUR' }]}
               />
             </div>
-          )}
+            <div style={{ width: '80%', marginBottom: '20px' }}>
+              <StatesField selectValue="USD" options={symbols} />
+            </div>
+            <div style={{ marginBottom: '20px', width: '80%' }}>
+              <RaisedButton
+                label="Calculate"
+                fullWidth
+                labelColor="#ffffff"
+                backgroundColor="#56D5C0"
+                onClick={() => calcRate(targetCurrency)}
+              />
+            </div>
+            <div style={{ marginBottom: '20px', width: '80%' }}>
+              <RaisedButton
+                label="Show Graph"
+                fullWidth
+                labelColor="#ffffff"
+                backgroundColor="#4568E5"
+                onClick={() => getGraph(targetCurrency)}
+              />
+            </div>
+            {convertedValue && (
+              <div
+                style={{
+                  border: '1px solid #000',
+                  padding: '10px',
+                  width: '76%'
+                }}
+              >
+                <NumberFormat
+                  value={parseFloat(amount)}
+                  suffix={' EUR = '}
+                  thousandSeparator
+                  displayType={'text'}
+                />
+                <NumberFormat
+                  value={parseFloat(convertedValue).toFixed(6)}
+                  suffix={` ${targetCurrency}`}
+                  thousandSeparator
+                  displayType={'text'}
+                />
+              </div>
+            )}
+            {historicalData.length > 0 && (
+              <div>
+                <AreaChart
+                  xType={'time'}
+                  axes
+                  width={700}
+                  height={500}
+                  areaColors={['#F3AF75']}
+                  data={[historicalData]}
+                />
+              </div>
+            )}
+          </div>
         </div>
         <div style={{ width: '50%' }}>
           <img src={coupleWithDog} style={{ width: '80%' }} />
--- src/components/NumberInput.js
+++ src/components/NumberInput.js
@@ -2,11 +2,12 @@ import React from 'react';
 import NumberFormat from 'react-number-format';
 import TextField from 'material-ui/TextField';
 
-const NumberInput = ({ onChange }) => (
+const NumberInput = ({ onChange, value }) => (
   <div>
     <NumberFormat
-      hintText="1"
-      format="### ### ### ### ### ### ###"
+      value={value}
+      thousandSeparator
+      allowNegative={false}
       onValueChange={values => onChange(values.value)}
       style={{ height: '34px' }}
     />
